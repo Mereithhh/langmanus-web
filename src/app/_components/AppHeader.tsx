88e116f054441fcecd16a1c1b171cf21599eaf21
mereith <wanglu@mereith.com>
2025-08-02 22:45:42 +0800

    fix: config ui

--- src/app/_components/AppHeader.tsx
+++ src/app/_components/AppHeader.tsx
@@ -1,14 +1,24 @@
 import { githubUrl, title } from "~/config";
+import { Github } from "~/core/icons";
 
 export function AppHeader() {
   return (
-    <div>
+    <div className="flex items-center justify-between w-full">
       <a
         className="font-serif text-lg font-extralight text-gray-500"
         href={githubUrl}
         target="_blank"
       >
         {title}
+      </a>
+
+      <a
+        href={githubUrl}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-gray-500 hover:text-gray-700 transition-colors"
+      >
+        <Github className="w-6 h-6" />
       </a>
     </div>
   );
